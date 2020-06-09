fc89250d86e60637043f4808824697597f15a87c
eric416 <eric23244@gmail.com>
2020-06-09 22:54:47 +0800

    image_function

--- image_f_test.py
+++ image_f_test.py
@@ -4,10 +4,10 @@ v='水滴.mp4'
 i='gbBvokN.jpg'
 
 frames=[]
-#frames,fps=image_process_function.get_video(v)
-frames=image_process_function.get_image(i)
-frames=image_process_function.color(frames,0.114,0.587,0.229)
+frames,fps=image_process_function.get_video(v)
+# frames=image_process_function.get_image(i)
+# frames=image_process_function.color(frames,0.114,0.587,0.229)
 frames=image_process_function.size(frames,50,50)
-#frames=image_process_function.gray(frames)
-#image_process_function.write_video('test.mp4',frames,fps)
-image_process_function.write_image('test.jpg',frames)
+frames=image_process_function.gray(frames)
+image_process_function.write_video('test.mp4',frames,fps)
+# image_process_function.write_image('test.jpg',frames)
