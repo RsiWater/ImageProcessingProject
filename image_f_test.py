d6cd8918261316d77ce3c718f291918ed43551f9
eric416 <eric23244@gmail.com>
2020-06-10 11:13:19 +0800

    renew

--- image_f_test.py
+++ image_f_test.py
@@ -1,13 +1,13 @@
 import image_process_function
 
 v='水滴.mp4'
-i='gbBvokN.jpg'
+i='./image_processing/patrick.jpg'
 
 frames=[]
-frames,fps=image_process_function.get_video(v)
-# frames=image_process_function.get_image(i)
-# frames=image_process_function.color(frames,0.114,0.587,0.229)
+#frames,fps=image_process_function.get_video(v)
+frames=image_process_function.get_image(i)
+#frames=image_process_function.color(frames,0.005,0.499,0.888)
 frames=image_process_function.size(frames,50,50)
 frames=image_process_function.gray(frames)
-image_process_function.write_video('test.mp4',frames,fps)
-# image_process_function.write_image('test.jpg',frames)
+#image_process_function.write_video('test.mp4',frames,fps)
+image_process_function.write_image('test.jpg',frames)
